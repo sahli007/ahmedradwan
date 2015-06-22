004d58f76e45ff22a04cb1c92e410726dd875265
Ahmed Radwan <ahmedrad@gmail.com>
2015-06-21 20:50:57 -0700

    improving scrolling

--- scripts/animationOnScroll.js
+++ scripts/animationOnScroll.js
@@ -5,59 +5,77 @@
     if (scrollTimer) {
         clearTimeout(scrollTimer);   // clear any previous pending timer
       }
-    scrollTimer = setTimeout(handleScroll, 100);   // set new timer
+    scrollTimer = setTimeout(handleScroll, 50);   // set new timer
   })
 
 
   var flag = true;
   var flag2 = true;
+  var sectionFlags = {
+    'first': true,
+    'second':true,
+    'third':true,
+    'fourth': true,
+    'fifth': true,
+    'sixth':true,
+  }
 
   function handleScroll() {
-    height = $(window).height();
-    if($(window).scrollTop() > 0.5*height)  {
+
+    var scrollPosition = $(window).scrollTop()
+    var height = $(window).height();
+    if(scrollPosition > 0.5*height && sectionFlags.first)  {
       $('#CS-div hr').addClass('spreadOutLine');
-      $('#CS-div img').addClass('fadeInDown animated displayON').removeClass('displayOFF');
-      $('#about-me-h1').addClass('fadeInDown animated displayON').removeClass('displayOFF');
+      $('#CS-div img, #about-me-h1').addClass('fadeInDown animated displayON').removeClass('displayOFF');
       $('#about-me-p').addClass('fadeInUp animated displayON').removeClass('displayOFF');
+      sectionFlags.first = false;
+      return;
     }
 
-    if($(window).scrollTop() > 1.5*height)  {
+    if(scrollPosition > 1.3*height && sectionFlags.second)  {
       $('.life-section i').addClass('fadeInDown animated displayON').removeClass('displayOFF');
-      $('#toggler a, #toggler').addClass('fadeInDown displayON').removeClass('displayOFF');
-      $('#mylife-div h2').addClass('fadeInDown animated displayON').removeClass('displayOFF');
+      $('#toggler a, #toggler, #mylife-div h2').addClass('fadeInDown animated displayON').removeClass('displayOFF');
       $('#work, #sections-wrapper').addClass('fadeInUp animated displayON').removeClass('displayOFF');
       if (flag2)
       {
         $('#workLine').css('width','85%');
         flag2 = false;
       }
-
+      sectionFlags.second = false;
+      return;
     }
 
-    if($(window).scrollTop() > 2.5 *height) {
+    if(scrollPosition > 2.3 *height && sectionFlags.third) {
       $('#skills-list-wrapper').addClass('displayON').removeClass('displayOFF');
       $('.fa-star').addClass('fadeInRight animated');
       $('#skills-list a, #skills-list i').addClass('fadeInLeft animated');
       $('#skills-list-wrapper h1, #skills-list-wrapper h2, #skills-list-wrapper h3').addClass('fadeInDown animated');
       $('#skills-list hr').addClass('spreadOutLine');
-
+      sectionFlags.third = false;
+      return;
     }
 
-    if($(window).scrollTop() > 3.5 *height) {
+    if(scrollPosition > 3.3 *height && sectionFlags.fourth) {
       $('#travel-box h2').addClass('fadeInLeft animated displayON').removeClass('displayOFF');
       $('#photo-box h2').addClass('fadeInRight animated displayON').removeClass('displayOFF');
       if(flag) {
         $('#travel-box').slideme('play');
         flag=false;
       }
+      sectionFlags.fourth = false;
+      return;
     }
 
-    if($(window).scrollTop() > 4.5 *height) {
+    if(scrollPosition > 4.3 *height && sectionFlags.fifth) {
       $('#map, #map h2').addClass('fadeIndown animated displayON').removeClass('displayOFF');
+      sectionFlags.fifth = false
+      return;
     }
 
-    if($(window).scrollTop() > 5.5 *height) {
+    if(scrollPosition > 5.3 *height && sectionFlags.sixth) {
       $('#waterfall-box h2').addClass('fadeIndown animated displayON').removeClass('displayOFF');
+      sectionFlags.sixth = false;
+      return;
     }
 
   };
